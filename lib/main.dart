dce4ae59931e610c4979d382e1e1139e72ae0f83
Jente Vets <C00148@brusselsairlines.com>
2020-01-14 14:34:30 +0100

    WIP Update design to material design and use tabview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:bbfutsal_app/model/gameresult.dart';
-import 'package:bbfutsal_app/ranking_page.dart';
+import 'package:bbfutsal_app/model/ranking.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
@@ -16,221 +16,267 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'KZVB App  ',
-      theme: new ThemeData(
-          primaryColor: Color.fromRGBO(58, 66, 86, 1.0),
-          canvasColor: Color.fromRGBO(58, 66, 86, 1.0)),
-      home: new ListPage(title: 'Results'),
-    );
+        title: 'KZVB App',
+        theme: new ThemeData(primaryColor: Colors.deepOrange),
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(text: "Results"),
+                  Tab(text: "Ranking")
+                  ],
+              ),
+              title: Text("KZVB App"),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                new ResultsList(title: 'Results'),
+                new RankingsList(title: 'Ranking'),
+              ],
+            ),
+          ),
+        )
+        );
   }
 }
 
-class ListPage extends StatefulWidget {
-  ListPage({Key key, this.title}) : super(key: key);
+class ResultsList extends StatefulWidget {
+  ResultsList({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _ListPageState createState() => _ListPageState();
+  _ResultsListState createState() => _ResultsListState();
 }
 
-class _ListPageState extends State<ListPage> {
-  List<GameResult> gameResults;
-  var dropdownValue = '2B';
-
-  @override
-  void initState() {
-    fetchGameResults(dropdownValue).then((result) {
-      setState(() {
-        gameResults = result;
-      });
-    });
-    super.initState();
-  }
+class _ResultsListState extends State<ResultsList> {
+  var defaultDivision = '2B';
 
   @override
   Widget build(BuildContext context) {
-    final topAppBar = AppBar(
-        elevation: 0.1,
-        backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-        title: Text(widget.title));
-
-    ListTile makeListTile(GameResult gameResult) => ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        leading: Container(
-            padding: EdgeInsets.only(right: 12.0),
-            decoration: new BoxDecoration(
-                border: new Border(
-                    right: new BorderSide(width: 1.0, color: Colors.white24))),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  new DateFormat('dd')
-                      .format(gameResult.date), //splittedDate[0],
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.normal),
-                ),
-                Text(
-                  new DateFormat('MM')
-                      .format(gameResult.date), //splittedDate[1],
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.normal),
-                ),
-              ],
-            )),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-                width: 100,
-                child: Text(
-                  gameResult.home,
-                  textAlign: TextAlign.left,
-                  textWidthBasis: TextWidthBasis.parent,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.normal),
-                )),
-            Container(
-                width: 55,
-                child: Text(
-                  gameResult.score,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                )),
-            Container(
-                width: 100,
-                child: Text(
-                  gameResult.visitors,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.normal),
-                ))
-          ],
-        ));
-
-    Card makeCard(GameResult gameResult) => Card(
-          elevation: 8.0,
-          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-          child: Container(
-            decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-            child: makeListTile(gameResult),
-          ),
-        );
-
-    final makeBody = Container(
-        child: Column(children: [
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-        Container(
-            width: 100,
-            child: Text(
-              "Division:",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.normal),
-            )),
-        DropdownButton<String>(
-          value: dropdownValue,
-          icon: Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          onChanged: (String newValue) {
-            fetchGameResults(newValue).then((result) {
-              setState(() {
-                gameResults = result;
-                dropdownValue = newValue;
-              });
-            });           
-          },
-          items: <String>['01', '2A', '2B', '3A', '3B', '3C']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-                value: value,
-                child: Text(
-                  value,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.normal),
-                ));
-          }).toList(),
-        )
-      ]),
-      Expanded(
-          child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: gameResults.length,
-        itemBuilder: (BuildContext context, int index) {
-          return makeCard(gameResults[index]);
-        },
-      ))
-    ]));
+    final topAppBar =
+        AppBar(backgroundColor: Colors.deepOrange, title: Text(widget.title));
 
     return Scaffold(
-      backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-      appBar: topAppBar,
-      body: makeBody,
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            ListTile(
-              title: Text('Results'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Ranking'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RankingPage()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+        appBar: topAppBar,
+        body: FutureBuilder(
+            future: fetchGameResults(defaultDivision),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return Center(child: Text("Loading..."));
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                          elevation: 8.0,
+                          margin: new EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 6.0),
+                          child: Container(
+                              child: ListTile(
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 20.0, vertical: 10.0),
+                                  leading: Container(
+                                      padding: EdgeInsets.only(right: 12.0),
+                                      decoration: new BoxDecoration(
+                                          border: new Border(
+                                              right:
+                                                  new BorderSide(width: 1.0))),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Text(
+                                            new DateFormat('dd').format(snapshot
+                                                .data[index]
+                                                .date), //splittedDate[0],
+                                            style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.normal),
+                                          ),
+                                          Text(
+                                            new DateFormat('MM').format(snapshot
+                                                .data[index]
+                                                .date), //splittedDate[1],
+                                            style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.normal),
+                                          ),
+                                        ],
+                                      )),
+                                  title: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Container(
+                                          width: 100,
+                                          child: Text(
+                                            snapshot.data[index].home,
+                                            textAlign: TextAlign.left,
+                                            textWidthBasis:
+                                                TextWidthBasis.parent,
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.normal),
+                                          )),
+                                      Container(
+                                          width: 55,
+                                          child: Text(
+                                            snapshot.data[index].score,
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.bold),
+                                          )),
+                                      Container(
+                                          width: 100,
+                                          child: Text(
+                                            snapshot.data[index].visitors,
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.normal),
+                                          ))
+                                    ],
+                                  ))));
+                    });
+              }
+            }));
+  }
+
+  Future<List<GameResult>> fetchGameResults(String division) async {
+    var secret = await secretFuture;
+    final response = await http.get(
+        'https://kzvb-datascraper.azurewebsites.net/api/results?code=' +
+            secret.resultsEndpointKey +
+            '&division=' +
+            division);
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      List responseJson = json.decode(response.body);
+      var gameResults =
+          responseJson.map((r) => GameResult.fromJson(r)).toList();
+      gameResults.sort((a, b) {
+        var adate = a.date;
+        var bdate = b.date;
+        return bdate.compareTo(
+            adate); //to get the order other way just switch `adate & bdate`
+      });
+
+      return gameResults;
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load game results.');
+    }
   }
 }
 
-Future<List<GameResult>> fetchGameResults(String division) async {
+class RankingsList extends StatefulWidget {
+  RankingsList({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _RankingsListState createState() => _RankingsListState();
+}
+
+class _RankingsListState extends State<RankingsList> {
+  var defaultDivision = '2B';
+
+  @override
+  Widget build(BuildContext context) {
+    final topAppBar = AppBar(elevation: 0.1, title: Text(widget.title));
+
+    return Scaffold(
+        appBar: topAppBar,
+        body: FutureBuilder(
+            future: fetchRanking(defaultDivision),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return Center(child: Text("Loading..."));
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                          elevation: 8.0,
+                          margin: new EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 6.0),
+                          child: Container(
+                              child: ListTile(
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 20.0, vertical: 10.0),
+                                  leading: Container(
+                                      padding: EdgeInsets.only(right: 12.0),
+                                      decoration: new BoxDecoration(
+                                          border: new Border(
+                                              right:
+                                                  new BorderSide(width: 1.0))),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Text(
+                                            snapshot.data[index].rank,
+                                            style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.normal),
+                                          )
+                                        ],
+                                      )),
+                                  title: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Container(
+                                          width: 100,
+                                          child: Text(
+                                            snapshot.data[index].name,
+                                            textAlign: TextAlign.left,
+                                            textWidthBasis:
+                                                TextWidthBasis.parent,
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.normal),
+                                          )),
+                                      Container(
+                                          width: 55,
+                                          child: Text(
+                                            snapshot.data[index].points,
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.bold),
+                                          )),
+                                    ],
+                                  ))));
+                    });
+              }
+            }));
+  }
+}
+
+Future<List<Ranking>> fetchRanking(String division) async {
   var secret = await secretFuture;
   final response = await http.get(
-      'https://kzvb-datascraper.azurewebsites.net/api/results?code=' + secret.resultsEndpointKey + '&division=' +
+      'https://kzvb-datascraper.azurewebsites.net/api/ranking?code=' +
+          secret.rankingEndpointKey +
+          '&division=' +
           division);
   if (response.statusCode == 200) {
     // If server returns an OK response, parse the JSON.
     List responseJson = json.decode(response.body);
-    var gameResults = responseJson.map((r) => GameResult.fromJson(r)).toList();
+    var gameResults = responseJson.map((r) => Ranking.fromJson(r)).toList();
     gameResults.sort((a, b) {
-      var adate = a.date;
-      var bdate = b.date;
-      return bdate.compareTo(
-          adate); //to get the order other way just switch `adate & bdate`
+      var aRank = a.rank;
+      var bRank = b.rank;
+      return aRank.compareTo(bRank);
     });
 
     return gameResults;
   } else {
     // If that response was not OK, throw an error.
-    throw Exception('Failed to load game results.');
+    throw Exception('Failed to load game rankings.');
   }
 }
--- lib/model/ranking.dart
+++ lib/model/ranking.dart
@@ -1,5 +1,7 @@
+import 'package:intl/intl.dart';
+
 class Ranking {
-  int rank;
+  String rank;
   String name;
   String points;
   String played;
@@ -7,11 +9,15 @@ class Ranking {
   Ranking({this.rank, this.name, this.points, this.played});
 
   factory Ranking.fromJson(Map<String, dynamic> json) {
-    return Ranking(
-      rank: json['ranking'],
+    var ranking = Ranking(
+      rank: null,
       name: json['clubName'],
       points: json['points'],
       played: json['gamesPlayed'],
     );
+
+    var f = NumberFormat("00");
+    ranking.rank = f.format(json['ranking']);
+    return ranking;
   }
 }
